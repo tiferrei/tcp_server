fb9eb4cf2aa072acb44b49b0cf504df8f16b873b
Tiago Ferreira <me@tiferrei.com>
2020-07-09 14:46:27 +0100

    feat: export Server type. This may not be a good idea. idk.

--- tcp_server.go
+++ tcp_server.go
@@ -10,11 +10,11 @@ import (
 // Client holds info about connection
 type Client struct {
 	conn   net.Conn
-	Server *server
+	server *Server
 }
 
-// TCP server
-type server struct {
+// TCP Server
+type Server struct {
 	address                  string // Address to open connection: localhost:9999
 	config                   *tls.Config
 	onNewClientCallback      func(c *Client)
@@ -24,16 +24,16 @@ type server struct {
 
 // Read client data from channel
 func (c *Client) listen() {
-	c.Server.onNewClientCallback(c)
+	c.server.onNewClientCallback(c)
 	reader := bufio.NewReader(c.conn)
 	for {
 		message, err := reader.ReadString('\n')
 		if err != nil {
 			c.conn.Close()
-			c.Server.onClientConnectionClosed(c, err)
+			c.server.onClientConnectionClosed(c, err)
 			return
 		}
-		c.Server.onNewMessage(c, message)
+		c.server.onNewMessage(c, message)
 	}
 }
 
@@ -57,23 +57,23 @@ func (c *Client) Close() error {
 	return c.conn.Close()
 }
 
-// Called right after server starts listening new client
-func (s *server) OnNewClient(callback func(c *Client)) {
+// Called right after Server starts listening new client
+func (s *Server) OnNewClient(callback func(c *Client)) {
 	s.onNewClientCallback = callback
 }
 
 // Called right after connection closed
-func (s *server) OnClientConnectionClosed(callback func(c *Client, err error)) {
+func (s *Server) OnClientConnectionClosed(callback func(c *Client, err error)) {
 	s.onClientConnectionClosed = callback
 }
 
 // Called when Client receives new message
-func (s *server) OnNewMessage(callback func(c *Client, message string)) {
+func (s *Server) OnNewMessage(callback func(c *Client, message string)) {
 	s.onNewMessage = callback
 }
 
-// Listen starts network server
-func (s *server) Listen() {
+// Listen starts network Server
+func (s *Server) Listen() {
 	var listener net.Listener
 	var err error
 	if s.config == nil {
@@ -82,7 +82,7 @@ func (s *server) Listen() {
 		listener, err = tls.Listen("tcp", s.address, s.config)
 	}
 	if err != nil {
-		log.Fatal("Error starting TCP server.")
+		log.Fatal("Error starting TCP Server.")
 	}
 	defer listener.Close()
 
@@ -90,41 +90,41 @@ func (s *server) Listen() {
 		conn, _ := listener.Accept()
 		client := &Client{
 			conn:   conn,
-			Server: s,
+			server: s,
 		}
 		go client.listen()
 	}
 }
 
-// Creates new tcp server instance
-func New(address string) *server {
-	log.Println("Creating server with address", address)
-	server := &server{
+// Creates new tcp Server instance
+func New(address string) *Server {
+	log.Println("Creating Server with address", address)
+	Server := &Server{
 		address: address,
 		config:  nil,
 	}
 
-	server.OnNewClient(func(c *Client) {})
-	server.OnNewMessage(func(c *Client, message string) {})
-	server.OnClientConnectionClosed(func(c *Client, err error) {})
+	Server.OnNewClient(func(c *Client) {})
+	Server.OnNewMessage(func(c *Client, message string) {})
+	Server.OnClientConnectionClosed(func(c *Client, err error) {})
 
-	return server
+	return Server
 }
 
-func NewWithTLS(address string, certFile string, keyFile string) *server {
-	log.Println("Creating server with address", address)
+func NewWithTLS(address string, certFile string, keyFile string) *Server {
+	log.Println("Creating Server with address", address)
 	cert, _ := tls.LoadX509KeyPair(certFile, keyFile)
 	config := tls.Config{
 		Certificates: []tls.Certificate{cert},
 	}
-	server := &server{
+	Server := &Server{
 		address: address,
 		config:  &config,
 	}
 
-	server.OnNewClient(func(c *Client) {})
-	server.OnNewMessage(func(c *Client, message string) {})
-	server.OnClientConnectionClosed(func(c *Client, err error) {})
+	Server.OnNewClient(func(c *Client) {})
+	Server.OnNewMessage(func(c *Client, message string) {})
+	Server.OnClientConnectionClosed(func(c *Client, err error) {})
 
-	return server
+	return Server
 }
